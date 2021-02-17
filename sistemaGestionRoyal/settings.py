6891305577e672df71adcfee86e7dc9a2ecfbc70
Jesus Alejandro Reynoso Mendoza <jealreynoso@gmail.com>
2021-02-17 09:50:39 -0700

    cambios

--- sistemaGestionRoyal/settings.py
+++ sistemaGestionRoyal/settings.py
@@ -143,5 +143,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'img')
+MEDIA_URL = '/static/img/'
