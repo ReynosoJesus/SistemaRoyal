4740097d40845fa436b6b558bb05b848a6bd7f52
Jesus Alejandro Reynoso Mendoza <jealreynoso@gmail.com>
2021-03-18 18:30:04 -0700

    cambios finales 1.0

--- sistemaGestionRoyal/settings.py
+++ sistemaGestionRoyal/settings.py
@@ -111,12 +111,12 @@ DATABASES = {
         'USER': 'dbadmin',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': ''
     }
 }
 
 #usuario produccion dbadmin, usuario debug/local ->postgres
-
+# port debug/local > 5432
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
